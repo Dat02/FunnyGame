b7d7a0970f920d36d13073fc6e4dfddb6aa264e8
Nguyễn Đình Đạt <datnguyendinh1311@gmail.com>
2022-04-21 16:25:45 +0700

    message

---
+++ src/main/java/Mypackage/Myframe.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mypackage;
+
+import java.awt.HeadlessException;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Admin
+ */
+public class Myframe extends JFrame{
+
+    public Myframe()  {
+    }
+
+   
+    
+    
+}
